b5d7d50209015493843b6a8398821ae63c0e03ed
pc <www.mmhmdmm83@gmail.com>
2022-03-18 21:22:04 +0330

    fix image to see in deploy

--- public/js/app.js
+++ public/js/app.js
@@ -1,43 +1,43 @@
 let allProducts = [{
         id: 1,
         title: 'ساعت هوشمند اپل سری ۶',
-        image: '/assets/images/image 7.png',
+        image: './assets/images/image 7.png',
         price: '۴,۸۲۵۰,۰۰۰'
     },
     {
         id: 2,
         title: 'ساعت هوشمند اپل سری ۶',
-        image: '/assets/images/apple-watch1.png',
+        image: './assets/images/apple-watch1.png',
         price: '۴,۸۲۵۰,۰۰۰'
     },
     {
         id: 3,
         title: 'ساعت هوشمند اپل سری ۶',
-        image: '/assets/images/image 7.png',
+        image: './assets/images/image 7.png',
         price: '۴,۸۲۵۰,۰۰۰'
     },
     {
         id: 4,
         title: 'ساعت هوشمند اپل سری ۶',
-        image: '/assets/images/apple-watch1.png',
+        image: './assets/images/apple-watch1.png',
         price: '۴,۸۲۵۰,۰۰۰'
     },
     {
         id: 5,
         title: 'ساعت هوشمند اپل سری ۶',
-        image: '/assets/images/image 7.png',
+        image: './assets/images/image 7.png',
         price: '۴,۸۲۵۰,۰۰۰'
     },
     {
         id: 7,
         title: 'ساعت هوشمند اپل سری ۶',
-        image: '/assets/images/apple-watch1.png',
+        image: './assets/images/apple-watch1.png',
         price: '۴,۸۲۵۰,۰۰۰'
     },
     {
         id: 8,
         title: 'ساعت هوشمند اپل سری ۶',
-        image: '/assets/images/image 7.png',
+        image: './assets/images/image 7.png',
         price: '۴,۸۲۵۰,۰۰۰'
     }
 ]
@@ -53,7 +53,7 @@ allProducts.forEach(item => {
     <div class="w-full bg-slate-200 rounded-xl py-4 flex justify-center items-center relative">
         <img class="w-16 md:w-24" src="${item.image}" alt="Product">
         <span class="flex items-center justify-center w-7 h-7 md:w-8 md:h-8 rounded-full bg-gray-300 absolute top-2 right-2 cursor-pointer">
-            <img class="w-4 h-4 sm:w-6 sm:h-6" src="/assets/images/Icon-Glyph-25-Love.png">
+            <img class="w-4 h-4 sm:w-6 sm:h-6" src="./assets/images/Icon-Glyph-25-Love.png">
         </span>
     </div>
     <!-- description -->
